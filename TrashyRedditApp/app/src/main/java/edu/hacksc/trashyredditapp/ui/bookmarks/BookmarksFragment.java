eb6786a64cc7fcc1706c9ff23bc450eba1c0fd84
Rain Cinco <rcinco@usc.edu>
2020-02-01 14:29:59 -0800

    bookmarks and profile pages created

---
+++ TrashyRedditApp/app/src/main/java/edu/hacksc/trashyredditapp/ui/bookmarks/BookmarksFragment.java
@@ -0,0 +1,35 @@
+package edu.hacksc.trashyredditapp.ui.bookmarks;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import edu.hacksc.trashyredditapp.R;
+
+public class BookmarksFragment extends Fragment {
+
+    private BookmarksViewModel bookmarksViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        bookmarksViewModel =
+                ViewModelProviders.of(this).get(BookmarksViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_bookmarks, container, false);
+        final TextView textView = root.findViewById(R.id.text_bookmarks);
+        bookmarksViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+}
