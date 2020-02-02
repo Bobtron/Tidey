14084fffc100e405851934338803db2b446bcbc2
Rain Cinco <rcinco@usc.edu>
2020-02-02 08:56:51 -0800

    change the w orld my final me ssage. Goodb ye

--- TrashyRedditApp/app/src/main/java/edu/hacksc/trashyredditapp/ui/profile/ProfileFragment.java
+++ TrashyRedditApp/app/src/main/java/edu/hacksc/trashyredditapp/ui/profile/ProfileFragment.java
@@ -119,20 +119,6 @@ public class ProfileFragment extends Fragment {
 
         eventArrayList = new ArrayList<Event>();
 
-        Map<String,?> keys = sharedPreferences.getAll();
-
-        for(Map.Entry<String,?> entry : keys.entrySet()){
-            if(entry.getKey().toString().contains("lat/lng:")) {
-                Log.d("map values", entry.getKey() + ": " +
-                        entry.getValue().toString());
-
-                LatLng latLng = Event.GetLatLng(entry.getKey());
-
-                Event event = new Event(user_id,latLng);
-                eventArrayList.add(event);
-            }
-        }
-
         //display_top.setText("Hello " + first + "!");
 
         // specify an adapter (see also next example)
